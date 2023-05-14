6ba5bfc650ea44a4695275edbbe095b53f5393e7
Gawkky <35524685+Gawkky@users.noreply.github.com>
2023-05-14 20:40:05 +0200

    fix .app redundancy and fill further .MD file

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
 
-applications = ["MultiViewer for F1.app", "Google Chrome.app",
-                "GitHub Desktop.app", "Spotify.app", "Visual Studio Code.app", "VLC.app"]
+applications = ["MultiViewer for F1", "Google Chrome",
+                "GitHub Desktop", "Spotify", "Visual Studio Code", "VLC"]
 
 for application in applications:
-    folder_path = f"/Applications/{application}/Contents/Resources/"
+    folder_path = f"/Applications/{application}.app/Contents/Resources/"
     for root, dirs, files in os.walk(folder_path):
         for file in files:
             if file.endswith(".lproj") and not file.startswith("base."):
