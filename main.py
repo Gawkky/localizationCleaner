c43f73b1bf6ef5551afa48de5f85f48993821385
Gawkky <35524685+Gawkky@users.noreply.github.com>
2023-05-14 20:42:51 +0200

    add comment to the main.py file and added to .MD

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 applications = ["MultiViewer for F1", "Google Chrome",
-                "GitHub Desktop", "Spotify", "Visual Studio Code", "VLC"]
+                "GitHub Desktop", "Spotify", "Visual Studio Code", "VLC"]  # Change these to your own apps
 
 for application in applications:
     folder_path = f"/Applications/{application}.app/Contents/Resources/"
